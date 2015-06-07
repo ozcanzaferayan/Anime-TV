089073e0037ccf93b8bbe065f555e26fb910a10c
Özcan Zafer AYAN <ozcanzaferayan@gmail.com>
2015-06-07 16:19:26 +0300

    New anime list caching implemented
    New anime list caching implemented in backgruond.
    Added anime notification implementing now

--- Anime TV/js/background.js
+++ Anime TV/js/background.js
@@ -2,59 +2,154 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+var bgTimeInterval = 15000; // 5dk'da bir
+var storage = chrome.storage.local;
+
+// Başlangıçta içeriklerin getirilmesi için
+$(document).ready(function(){
+  storeNewAnimes();
+});
+
+setInterval(function(){
+  console.log("Servis çalışıyor: " + new Date());
+  storeNewAnimes();
+}, bgTimeInterval);
+
+function storeNewAnimes(){
+  // Storage'da kaydedilen önceki animeleri getir
+  storage.get('newAnimes', function (data) {
+    // Eğer veriler storage'da yoksa yenilerini API'den çek
+    if($.isEmptyObject(data) || chrome.runtime.lastError) {
+      console.log("newAnimes verisi yok");
+      getNewAnimes(data);
+     }
+    // Eğer veriler storage'da varsa güncellemek için kontrol et
+    else {
+      console.log('newAnimes verisi cachede');
+      var nextRunTime = new Date(data.newAnimes.nextrun).getTime();
+      var currentTime = new Date().getTime();
+      // Eğer API'nin sonraki çalışma zamanı gelmişse yeni animeleri getir
+      if (currentTime >= nextRunTime){
+        getNewAnimes(data);
+      }
+      else {
+        console.log('Henüz API güncellenmedi. nextrun: ' + new Date(nextRunTime));
+      }
+    }
+  });
+}
+
+// API'den yeni animeleri çekip storage'a set etmek içindir
+function getNewAnimes(oldAnimes){
+  $.ajax({
+    url:"https://www.kimonolabs.com/api/cq7nr4ag?apikey=bq34GvSZDidJFU4L4Kp7chJoJRvT0LSR",
+    crossDomain: true,
+    dataType: "jsonp",
+    success: function (response) {
+      console.log(response);
+      newAnimes = response;
+      console.log(newAnimes);
+      storage.set({'newAnimes': newAnimes});
+      checkForNewAnimes(oldAnimes, newAnimes);
+    },
+    error: function (xhr, status) {
+      
+    }
+  });
+}
+
+// Yeni gelen anime listesinde eski listeden farklı anime var mı
+function checkForNewAnimes(oldAnimes, newAnimes){
+  var addedAnimes = [];
+  if ($.isEmptyObject(oldAnimes)) return;
+  var oldAnimeList = oldAnimes.results.anime;
+  var newAnimeList = newAnimes.results.anime;
+  $(newAnimeList).each(function(i,v){
+    var isFind = false;
+    $(oldAnimeList).each(function(j,y){
+      var newAnimeName = v.name.text;
+      var oldAnimeName = y.name.text;
+      if(oldAnimeName == newAnimeName){
+        isFind = true;
+        return false;
+      }
+    });
+    if(!isFind){
+      addedAnimes.push();
+      notificateAnimes(addedAnimes);
+    }
+  });
+}
+
+// Yeni eklenen animeleri bildirim olarak göstermek için
+function notificateAnimes(addedAnimes){
+  $(addedAnimes).each(function(i,v){  
+    var animeName = v.name.text;
+    var animeIcon;
+    var notTitle = timeText;          // Notification title
+    var notIcon = '../img/icon.png';  // Notification icon
+    var notBody = 'Yeni anime geldi!'  // Notification text
+    var notTimeout = 5000;            // 5sn sonra kapanacak
+    var notification = new Notification(
+      notTitle, {
+        icon: notIcon,
+        body: notBody
+      });
+
+    setTimeout(function(){
+      notification.close();
+    }); 
+  });
+}
+
 /*
   Displays a notification with the current time. Requires "notifications"
   permission in the manifest file (or calling
   "Notification.requestPermission" beforehand).
 */
-function show() {
-  var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
-  var hour = time[1] % 12 || 12;               // The prettyprinted hour.
-  var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
-  var timeText = hour + time[2] + ' ' + period;
-  var notTitle = timeText;
-  var notIcon = '../img/icon.png';
-  var notBody = 'Yeni anime geldi'
-  var notification = new Notification(
-    notTitle, {
-      icon: notIcon,
-      body: notBody
-    });
-  setTimeout(function(){
-    notification.close();
-  },5000);
+// function show() {
+//   var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
+//   var hour = time[1] % 12 || 12;               // The prettyprinted hour.
+//   var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
+//   var timeText = hour + time[2] + ' ' + period;
+//   var notTitle = timeText;          // Notification title
+//   var notIcon = '../img/icon.png';  // Notification icon
+//   var notBody = 'Yeni anime geldi'  // Notification text
+//   var notTimeout = 5000;            // 5sn sonra kapanacak
+//   var notification = new Notification(
+//     notTitle, {
+//       icon: notIcon,
+//       body: notBody
+//     });
 
-  // var notification = webkitNotifications.createNotification(
-  //   '../img/icon.png',
-  //   hour + time[2] + ' ' + period,
-  //   "Biswarup Adhikari Notification"
-  // );
-  // notification.show();
+//   setTimeout(function(){
+//     notification.close();
+//   }); 
   
-}
+// }
 
-// Conditionally initialize the options.
-if (!localStorage.isInitialized) {
-  localStorage.isActivated = true;   // The display activation.
-  localStorage.frequency = 1;        // The display frequency, in minutes.
-  localStorage.isInitialized = true; // The option initialization.
-}
+// // Conditionally initialize the options.
+// if (!localStorage.isInitialized) {
+//   localStorage.isActivated = true;   // The display activation.
+//   localStorage.frequency = 1;        // The display frequency, in minutes.
+//   localStorage.isInitialized = true; // The option initialization.
+// }
 
-// Test for notification support.
-if (window.Notification) {
-  // While activated, show notifications at the display frequency.
-  if (JSON.parse(localStorage.isActivated)) { show(); }
+// // Test for notification support.
+// if (window.Notification) {
+//   // While activated, show notifications at the display frequency.
+//   if (JSON.parse(localStorage.isActivated)) { show(); }
 
-  var interval = 0; // The display interval, in minutes.
+//   var interval = 0; // The display interval, in minutes.
 
-  setInterval(function() {
-    interval++;
+//   setInterval(function() {
+//     interval++;
 
-    if (JSON.parse(localStorage.isActivated) &&
-        localStorage.frequency <= interval) 
-    {
-      show();
-      interval = 0;
-    }
-  }, 300000); // 5dk
-}
+//     if (JSON.parse(localStorage.isActivated) &&
+//         localStorage.frequency <= interval) 
+//     {
+//       show();
+//       interval = 0;
+//     }
+//   }, bgTimeInterval); // 5dk
+// }
