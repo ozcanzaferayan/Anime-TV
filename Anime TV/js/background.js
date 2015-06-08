05b860ca0804e7f7fc5a60b9ee0afb0fb9b3bc48
Özcan Zafer AYAN <ozcanzaferayan@gmail.com>
2015-06-08 03:37:57 +0300

    New anime list caching bugs eliminated

--- Anime TV/js/background.js
+++ Anime TV/js/background.js
@@ -4,6 +4,7 @@
 
 var bgTimeInterval = 15000; // 5dk'da bir
 var storage = chrome.storage.local;
+var newAnimes;
 
 // Başlangıçta içeriklerin getirilmesi için
 $(document).ready(function(){
@@ -11,29 +12,46 @@ $(document).ready(function(){
 });
 
 setInterval(function(){
-  console.log("Servis çalışıyor: " + new Date());
+  var currentTimeText = /(..)(:..)(:..)/.exec(new Date())[0];
+  console.log("Servis çalışıyor: " + currentTimeText);
   storeNewAnimes();
 }, bgTimeInterval);
 
 function storeNewAnimes(){
   // Storage'da kaydedilen önceki animeleri getir
-  storage.get('newAnimes', function (data) {
+  storage.get(newAnimes, function (data) {
     // Eğer veriler storage'da yoksa yenilerini API'den çek
     if($.isEmptyObject(data) || chrome.runtime.lastError) {
-      console.log("newAnimes verisi yok");
+      console.log("newAnimes verisi storage'da yok.");
       getNewAnimes(data);
      }
     // Eğer veriler storage'da varsa güncellemek için kontrol et
     else {
-      console.log('newAnimes verisi cachede');
-      var nextRunTime = new Date(data.newAnimes.nextrun).getTime();
+      console.log("newAnimes storage'da var:");
+      console.log(data);
+      if (data.lastrunstatus == "in progress") {
+          // api şu anki yenilenme zamanı
+          var thisVersionRunMilis = new Date(newAnimes.thisversionrun).getTime();
+          var apiFrequencyMilis = 15 * 60 * 1000; // 15dk
+          // 15 dk sonrasında api tekrar yenilenecek
+          data.nextrun = new Date(thisVersionRunMilis + apiFrequencyMilis);
+          // api isteği tamamlandı
+          data.lastrunstatus == "success";
+          newAnimes = data;
+          storage.set(newAnimes);
+      }
+      else { // "success"
+
+      }
+      var nextRunTime = new Date(data.nextrun).getTime();
       var currentTime = new Date().getTime();
       // Eğer API'nin sonraki çalışma zamanı gelmişse yeni animeleri getir
       if (currentTime >= nextRunTime){
         getNewAnimes(data);
       }
       else {
-        console.log('Henüz API güncellenmedi. nextrun: ' + new Date(nextRunTime));
+        var nextRunTimeText = /(..)(:..)(:..)/.exec(new Date(nextRunTime))[0];
+        console.log('Henüz API güncellenmedi. nextrun: ' + nextRunTimeText);
       }
     }
   });
@@ -46,10 +64,12 @@ function getNewAnimes(oldAnimes){
     crossDomain: true,
     dataType: "jsonp",
     success: function (response) {
-      console.log(response);
       newAnimes = response;
+      console.log('newAnimes:');
       console.log(newAnimes);
-      storage.set({'newAnimes': newAnimes});
+      console.log('oldAnimes:');
+      console.log(oldAnimes);
+      storage.set(newAnimes);
       checkForNewAnimes(oldAnimes, newAnimes);
     },
     error: function (xhr, status) {
@@ -74,31 +94,37 @@ function checkForNewAnimes(oldAnimes, newAnimes){
         return false;
       }
     });
+    // Yeni anime eski anime listesinde yoksa 
     if(!isFind){
-      addedAnimes.push();
-      notificateAnimes(addedAnimes);
+      addedAnimes.push(v);
     }
   });
+  notificateAnimes(addedAnimes);
 }
 
 // Yeni eklenen animeleri bildirim olarak göstermek için
 function notificateAnimes(addedAnimes){
   $(addedAnimes).each(function(i,v){  
-    var animeName = v.name.text;
-    var animeIcon;
-    var notTitle = timeText;          // Notification title
-    var notIcon = '../img/icon.png';  // Notification icon
-    var notBody = 'Yeni anime geldi!'  // Notification text
-    var notTimeout = 5000;            // 5sn sonra kapanacak
+    var animeName = v.name.text;        // Anime adı
+    var animeLink = v.text.href;        // Anime linki
+    var animeIcon = v.image.src;        // Anime icon linki
+    var notTitle = timeText;            // Notification adı
+    var notIcon = animeIcon;            // Notification icon
+    var notBody = 'Yeni anime geldi!';  // Notification text
+    var notTimeout = 5000;              // 5sn sonra kapanacak
     var notification = new Notification(
       notTitle, {
         icon: notIcon,
         body: notBody
       });
-
+    // Notification'a tıklanınca anime'nin linkine gidilmesi için
+    notification.addEventListener('click', function() {
+      notification.close();
+      window.open(animeLink);
+    });
     setTimeout(function(){
       notification.close();
-    }); 
+    }, notTimeout); 
   });
 }
 
