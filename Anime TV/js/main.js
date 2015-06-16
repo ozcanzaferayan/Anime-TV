18a0c3f9093b3764b33ad99845b8615caa08f19e
Özcan Zafer AYAN <ozcanzaferayan@gmail.com>
2015-06-17 00:06:08 +0300

    Date comparison fixed

--- Anime TV/js/main.js
+++ Anime TV/js/main.js
@@ -1,5 +1,8 @@
 // Strips possible html tags from content
 function stripHTML(html) {
+	if (html === undefined)
+		return("");
+	html = html.toString();
 	// Regex for html tags
 	var re = /(<([^>]+)>)/ig;
 	var txt = html.replace(re, "");
